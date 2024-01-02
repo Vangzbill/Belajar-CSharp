c29585da1a83ce83b8837e98ebdd88e88b967e21
Sabilla Luthfi Rahmadhan <mamadhan017@gmail.com>
2024-01-02 20:42:22 +0700

    Make simple calculator

---
+++ SimpleCalculator/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SimpleCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Simple Calculator!");
+            Console.WriteLine("Please enter your first number: ");
+            string firstNumber = Console.ReadLine();
+            Console.WriteLine("Please enter your second number: ");
+            string secondNumber = Console.ReadLine();
+            Console.WriteLine("Please enter your operation: ");
+            string operation = Console.ReadLine();
+            int firstNumberInt = Convert.ToInt32(firstNumber);
+            int secondNumberInt = Convert.ToInt32(secondNumber);
+            if (operation == "+")
+            {
+                Console.WriteLine(firstNumberInt + secondNumberInt);
+            }
+            else if (operation == "-")
+            {
+                Console.WriteLine(firstNumberInt - secondNumberInt);
+            }
+            else if (operation == "*")
+            {
+                Console.WriteLine(firstNumberInt * secondNumberInt);
+            }
+            else if (operation == "/")
+            {
+                Console.WriteLine(firstNumberInt / secondNumberInt);
+            }
+            else
+            {
+                Console.WriteLine("Invalid operation!");
+            }
+        }
+    }
+}
